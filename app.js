76757d3e106fe3b30cbab9440a0e0068fd6b6a31
unknown <ajaditi0311@gmail.com>
2020-09-23 03:33:46 +0530

    Added Mongodb and description in campgrounds

--- app.js
+++ app.js
@@ -1,9 +1,41 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
+var express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+require('dotenv').config();
+
+const uri = process.env.MONGO_URL;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");
+});
+
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+/*Campground.create(
+    {
+        name: "Northway", 
+        image: "https://images.unsplash.com/photo-1582908140887-5935bade88da?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+        description: "Mountains all around"
+    }, function(err, campground) {
+        if(err) {
+            console.log(err);
+        } else {
+            console.log("Newly created campground");
+            console.log(campground); 
+        }
+    });*/
 
 var campgrounds = [
     {name: "Great Outdoors",image: "https://images.unsplash.com/photo-1507163525711-618d70c7a8f1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"},
@@ -16,21 +48,43 @@ app.get("/", function(req, res){
 });
 
 app.get("/campgrounds", function(req,res){
-    res.render("campgrounds",{campgrounds: campgrounds});
+    Campground.find({}, function(err, allCampgrounds){
+        if(err) {
+            console.log(err);
+        } else {
+            res.render("index",{campgrounds: allCampgrounds});
+        }
+    });
 });
 
 app.post("/campgrounds", function(req,res){
     var name = req.body.name;
     var image = req.body.image;
-    var newCampground = {name: name, image: image};
-    campgrounds.push(newCampground);
-    res.redirect("/campgrounds");
+    var desc = req.body.description;
+    var newCampground = {name: name, image: image, description: desc};
+    Campground.create(newCampground, function(err, newlyCreated){
+        if(err) {
+            console.log(err);
+        } else {
+            res.redirect("/campgrounds");
+        }
+    });
 });
 
 app.get("/campgrounds/new", function(req, res){
     res.render("new");
 });
 
-app.listen(3000, function(req, res){
-    console.log("Yelpcamp server has started");
+app.get("/campgrounds/:id", function(req, res){
+    Campground.findById(req.params.id, function(err, foundCampground) {
+        if(err) {
+            console.log(err);
+        } else {
+            res.render("show",{campground: foundCampground});
+        }
+    });
 });
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));

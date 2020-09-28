9cb842361608553ab1b120dfb3722e0562807bad
unknown <ajaditi0311@gmail.com>
2020-09-29 02:13:22 +0530

    Refactored routes

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ var express = require("express"),
     User = require("./models/user"),
     seedDB = require("./seeds");
 
+var commentRoutes = require("./routes/comments"),
+    campgroundRoutes = require("./routes/campgrounds"),
+    indexRoutes = require("./routes/index");
+
 seedDB();
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
@@ -40,6 +44,10 @@ app.use(function(req, res, next) {
     next();
 });
 
+app.use("/", indexRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/comments", commentRoutes);
+
 /*Campground.create(
     {
         name: "Northway", 
@@ -59,113 +67,6 @@ var campgrounds = [
     {name: "Northway", image: "https://images.unsplash.com/photo-1582908140887-5935bade88da?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"},
     {name: "Tripp Lake", image: "https://images.unsplash.com/photo-1506535995048-638aa1b62b77?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"}
 ];
-
-app.get("/", function(req, res){
-    res.render("landing");
-});
-
-app.get("/campgrounds", function(req,res){
-    Campground.find({}, function(err, allCampgrounds){
-        if(err) {
-            console.log(err);
-        } else {
-            res.render("campgrounds/index",{campgrounds: allCampgrounds, currentUser: req.user});
-        }
-    });
-});
-
-app.post("/campgrounds", function(req,res){
-    var name = req.body.name;
-    var image = req.body.image;
-    var desc = req.body.description;
-    var newCampground = {name: name, image: image, description: desc};
-    Campground.create(newCampground, function(err, newlyCreated){
-        if(err) {
-            console.log(err);
-        } else {
-            res.redirect("/campgrounds");
-        }
-    });
-});
-
-app.get("/campgrounds/new", function(req, res){
-    res.render("campgrounds/new");
-});
-
-app.get("/campgrounds/:id", function(req, res){
-    Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground) {
-        if(err) {
-            console.log(err);
-        } else {
-            res.render("campgrounds/show",{campground: foundCampground});
-        }
-    });
-});
-
-app.get("/campgrounds/:id/comments/new", isLoggedIn, function(req, res) {
-    Campground.findById(req.params.id, function(err, campground) {
-        if(err) {
-            console.log(err);
-        } else {
-            res.render("comments/new", {campground: campground});
-        }
-    });
-});
-
-app.post("/campgrounds/:id/comments", isLoggedIn, function(req, res) {
-    Campground.findById(req.params.id, function(err, campground) {
-        if(err) {
-            console.log(err);
-            res.redirect("/campgrounds");
-        } else {
-            Comment.create(req.body.comment, function(err, comment) {
-                campground.comments.push(comment);
-                campground.save();
-                res.redirect("/campgrounds/"+campground._id);
-            });
-        }
-    });
-});
-
-app.get("/register", function(req, res) {
-    res.render("register");
-});
-
-app.post("/register", function(req, res) {
-    var newUser = new User({username: req.body.username});
-    User.register(newUser, req.body.password, function(err, user) {
-        if(err) {
-            console.log(err);
-            return res.render("register");
-        }
-        passport.authenticate("local")(req, res, function() {
-            res.redirect("/campgrounds");
-        });
-    });
-});
-
-app.get("/login", function(req, res) {
-    res.render("login");
-});
-
-app.post("/login", passport.authenticate("local",
-    {
-        successRedirect: "/campgrounds",
-        failureRedirect: "/login"
-    }), function(req, res) {
-});
-
-app.get("/logout", function(req, res) {
-    req.logout();
-    res.redirect("/campgrounds");
-});
-
-function isLoggedIn(req, res, next) {
-    if(req.isAuthenticated()) {
-        return next();
-    }
-    res.redirect("/login");
-}
 
 const port = process.env.PORT || 3000;
 

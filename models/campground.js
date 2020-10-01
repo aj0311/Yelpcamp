07dbccdb7eb21c123452cd0195c131816b757f87
unknown <ajaditi0311@gmail.com>
2020-10-02 04:20:07 +0530

    Added feature of dynamic pricing of a campground

--- models/campground.js
+++ models/campground.js
@@ -3,6 +3,7 @@ const comment = require("./comment");
 
 var campgroundSchema = new mongoose.Schema({
     name: String,
+    price: String,
     image: String,
     description: String,
     author : {
